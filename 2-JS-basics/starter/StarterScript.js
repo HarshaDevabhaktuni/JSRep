f92609a7128664e3469c6ae46219ead2054b1a40
HarshaDevabhaktuni <devabhaktuni.harsha@gmail.com>
2020-11-14 03:56:59 +0530

    commit on 'master'

--- 2-JS-basics/starter/StarterScript.js
+++ 2-JS-basics/starter/StarterScript.js
@@ -2,4 +2,5 @@ let country='India';
 let continent='Asia';
 let population='1.3 billion';
 console.log("I'm from " + country + ". It's in " + continent + ". It has a total population of " +population + "!");
+
 alert("I'm from " + country + ". It's in " + continent + ". It has a total population of " +population + "!");
